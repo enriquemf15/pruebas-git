f26f062c596aec6bafe03e85ef4ad37d5c31d639
EnriqueMF <enrique10091995@gmail.com>
2020-11-26 19:19:41 +0100

    Delete Octal and Binary prints

--- ASCIITable/ASCIITable.ino
+++ ASCIITable/ASCIITable.ino
@@ -57,14 +57,6 @@ void loop() {
   // prints value as string in hexadecimal (base 16):
   Serial.print(thisByte, HEX);
 
-  Serial.print(", oct: ");
-  // prints value as string in octal (base 8);
-  Serial.print(thisByte, OCT);
-
-  Serial.print(", bin: ");
-  // prints value as string in binary (base 2) also prints ending line break:
-  Serial.println(thisByte, BIN);
-
   // if printed last visible character '~' or 126, stop:
   if (thisByte == 126) {    // you could also use if (thisByte == '~') {
     // This loop loops forever and does nothing
